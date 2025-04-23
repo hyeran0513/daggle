c8fea90948a607659b496716f6db661a27255bcf
hyeran0513 <hr_0513@naver.com>
2025-04-23 16:06:36 +0900

    feat: 버튼 컴포넌트 추가

---
+++ src/components/Button.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = ({
+  children,
+  size = "medium",
+  variant = "black",
+  disabled = false,
+  ...props
+}) => {
+  return (
+    <StyledButton size={size} variant={variant} disabled={disabled} {...props}>
+      {children}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 150%;
+  letter-spacing: -0.3%;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.white};
+  width: ${({ theme, size }) => theme.buttons.sizes[size].width};
+  height: ${({ theme, size }) => theme.buttons.sizes[size].height};
+  background-color: ${({ theme, variant }) =>
+    theme.buttons.colors[variant].default};
+
+  &:hover {
+    background-color: ${({ theme, variant }) =>
+      theme.buttons.colors[variant].hover};
+  }
+
+  &:active {
+    background-color: ${({ theme, variant }) =>
+      theme.buttons.colors[variant].pressed};
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.component.natural};
+    cursor: not-allowed;
+  }
+`;
+
+export default Button;
