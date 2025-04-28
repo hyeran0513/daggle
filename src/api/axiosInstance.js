fd8866190c92aa61ec010d3e976df05a89fd9f0c
hyeran0513 <hr_0513@naver.com>
2025-04-28 19:46:53 +0900

    refactor: axiosInstance.js에서 return Promise.reject(error); 삭제

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -51,7 +51,6 @@ axiosInstance.interceptors.response.use(
           console.error("토큰 갱신 오류: ", error);
           authStore.getState().logout();
           alert("세션이 만료되었습니다. 다시 로그인해주세요.");
-          return Promise.reject(error);
         }
       }
     }
