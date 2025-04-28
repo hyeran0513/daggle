259ab9ac78aa9ded69853757e54360f16a04c725
hyeran0513 <hr_0513@naver.com>
2025-04-28 17:13:10 +0900

    chore: SEO 처리 - react-helmet에서 react-helmet-async로 변경

--- src/components/atoms/SEO.jsx
+++ src/components/atoms/SEO.jsx
@@ -1,27 +1,22 @@
-import React, { useEffect } from "react";
-import { Helmet } from "react-helmet";
-import { useLocation } from "react-router-dom";
+import React from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const SEO = ({ title, description, keywords }) => {
-  const location = useLocation();
-
-  useEffect(() => {
-    document.title = title;
-  }, [location, title]);
-
   return (
-    <Helmet>
-      {/* 페이지 제목 */}
-      <title>{title}</title>
+    <HelmetProvider>
+      <Helmet>
+        {/* 페이지 제목 */}
+        <title>{title}</title>
 
-      {/* 일반 메타 태그 */}
-      <meta name="description" content={description} />
-      <meta name="keywords" content={keywords} />
+        {/* 일반 메타 태그 */}
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
 
-      {/* Open Graph 메타 태그 */}
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-    </Helmet>
+        {/* Open Graph 메타 태그 */}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+      </Helmet>
+    </HelmetProvider>
   );
 };
 
