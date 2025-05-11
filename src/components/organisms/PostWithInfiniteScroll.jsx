18ee92bcfc811b2826b2a154cb2f651f81c0c289
hyeran0513 <hr_0513@naver.com>
2025-05-11 19:55:06 +0900

    fix: 마운트 시 중복 호출 방지를 위해 removeQueries 대신 invalidateQueries 사용

--- src/components/organisms/PostWithInfiniteScroll.jsx
+++ src/components/organisms/PostWithInfiniteScroll.jsx
@@ -19,10 +19,13 @@ const PostWithInfiniteScroll = ({ isMobile }) => {
     refetch,
   } = useInfinitePosts({ limit: 10 });
 
-  // 다른 페이지에서 현재 페이지로 이동될 때, infinitePosts 쿼리 데이터를 초기화
+  // 컴포넌트 언마운트 시 infinitePosts 쿼리 무효화
+  // 다시 방문 시 refetch 유도
   useEffect(() => {
-    queryClient.removeQueries(["infinitePosts"]);
-  }, [queryClient]);
+    return () => {
+      queryClient.invalidateQueries(["infinitePosts"]);
+    };
+  }, []);
 
   // 화면 크기 변경 시 데이터 리패칭
   useEffect(() => {
@@ -40,9 +43,9 @@ const PostWithInfiniteScroll = ({ isMobile }) => {
 
   return (
     <>
-      {infinitePosts?.pages.length > 0 ? (
+      {infinitePosts?.pages?.length > 0 ? (
         <PostCardList
-          posts={infinitePosts?.pages.flatMap((page) => page.items)}
+          posts={infinitePosts?.pages?.flatMap((page) => page.items)}
         />
       ) : (
         <NoData text="게시글이 없습니다." />
