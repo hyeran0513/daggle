3c4ae976ba2a56663c896a69b45e406133ec01d3
hyeran0513 <hr_0513@naver.com>
2025-05-12 13:57:37 +0900

    fix: useResponsive에 ssr: false 옵션 추가

--- src/hooks/useResponsive.jsx
+++ src/hooks/useResponsive.jsx
@@ -4,12 +4,15 @@ import { theme } from "../styles/theme";
 export const useResponsive = () => {
   const isMobile = useMediaQuery({
     maxWidth: parseInt(theme.breakpoints.mobile),
+    ssr: false,
   });
   const isTablet = useMediaQuery({
     maxWidth: parseInt(theme.breakpoints.tablet),
+    ssr: false,
   });
   const isDesktop = useMediaQuery({
     minWidth: parseInt(theme.breakpoints.web),
+    ssr: false,
   });
 
   return { isMobile, isTablet, isDesktop };
